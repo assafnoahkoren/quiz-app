dae2dd32762b7cb45254c61c5ed6b7c1248842d9
Assaf Koren <assafnoahkoren@gmail.com>
2024-05-28 20:56:37 +0300

    added ionic app

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'koren.com.quiz',
+  appName: 'quiz-mobile',
+  webDir: 'dist'
+};
+
+export default config;
